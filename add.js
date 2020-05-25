c55d01c1f17fc8b283ba882453b1db372fa923fd
Muhammad Abban Siddiqui <abbansiddiqui456@gmail.com>
2020-05-25 15:58:38 +0500

    deploy

---
+++ add.js
@@ -0,0 +1,11 @@
+
+//   add to cart cardimg
+ 
+$('input[type=number]').inputspinner({
+    incrementButton: '<b>&gt;<b>',
+    decrementButton: '<b>&lt;<b>',
+    groupClass: 'input-group-md',
+    buttonClass: 'btn-light',
+    buttonWidth: '2rem'
+});
+
